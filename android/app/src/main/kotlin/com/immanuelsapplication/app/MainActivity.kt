41b7a0c34e782dd6fcb0bcae902a5e915f5df0f2
MarcellinoDani <165283575+MarcellinoDani@users.noreply.github.com>
2024-07-07 23:45:29 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/immanuelsapplication/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.immanuelsapplication.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: must_be_immutable
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+// iPhone 14 & 15 Pro Max - Two images
+  static String imgImage4 = '$imagePath/img_image_4.png';
+
+// iPhone 14 & 15 Pro Max - Four images
+  static String imgImage6 = '$imagePath/img_image_6.png';
+
+// iPhone 14 & 15 Pro Max - Five images
+  static String imgGoogle = '$imagePath/img_google.png';
+
+  static String imgFacebook = '$imagePath/img_facebook.png';
+
+  static String imgGithub = '$imagePath/img_github.png';
+
+// iPhone 14 & 15 Pro Max - Ten images
+  static String imgEdit = '$imagePath/img_edit.png';
+
+// Common images
+  static String imgVector = '$imagePath/img_vector.svg';
+
+  static String imgVectorPrimary = '$imagePath/img_vector_primary.svg';
+
+  static String imgCheckmark = '$imagePath/img_checkmark.svg';
+
+  static String imgLocation = '$imagePath/img_location.svg';
+
+  static String imgEye = '$imagePath/img_eye.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
